9ab85cc6e275a2777ca17e7578f32004be31412d
Miles Johnson <mileswjohnson@gmail.com>
2023-08-15 15:04:23 -0700

    Add globals.

---
+++ tests/globals_test.rs
@@ -0,0 +1,6 @@
+// use proto_pdk_test_utils::*;
+// use starbase_sandbox::create_empty_sandbox;
+
+// This won't work because we change the home directory for the sandbox,
+// but the `bun` command writes to the original home directory.
+// generate_globals_test!("bun-test", "cowsay");
